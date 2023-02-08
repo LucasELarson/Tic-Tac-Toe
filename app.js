31710467ae0a14f93598b422be0420c44fb6bd95
Lucas <larsonlucas112@gmail.com>
2023-02-08 13:15:38 -0600

    Added "change letter on click" only if square.txt = ''

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 /* eslint-disable no-unused-vars */
 // winning patterns: 123   456   789   147   258   369   159   753 //
+const patternArray = [];
+
+// Changes the letter in the box //
+function makePlay(x) {
+   let playLetter = "X";
+   if (document.getElementById(x).innerText !== "") {
+      alert("Please Choose A Different Square");
+   } else {
+      document.getElementById(x).innerText = playLetter;
+      if (playLetter === "X") {
+         playLetter = "O";
+      } else {
+         playLetter = "X";
+      }
+   }
+}
